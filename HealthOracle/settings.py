f7c2a3708eb6fbc39edf2bb6c774fc7c426688e8
Dhruv852 <dhruvtiwari.21.2004@gmail.com>
2025-12-23 11:17:11 +0530

    updated api key

--- HealthOracle/settings.py
+++ HealthOracle/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 
 
 # Gemini API Configuration
-GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', 'AIzaSyBhOzZgzBVBRufTJySxC6zhuFnum8RFgu0')
+GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', 'AIzaSyBg0erT664pZ_fQCcGQhqzsmRbnR-gfPDQ')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
